aea66bd300094e20051bc04a25b256dc640c0997
Falutin Raven <falutinraven@gmail.com>
2024-12-02 17:45:47 -0600

    get button clicks to show up in display

--- src/main.js
+++ src/main.js
@@ -29,6 +29,41 @@ function operate(operand1, operator, operand2){
     }
 }
 
+function create_digits(){
+    container = document.querySelector("#digits");
+    for(var i = 1; i < 10; i++){
+        let digit = document.createElement("button");
+        digit.classList.add(`digit`);
+        digit.textContent = i;
+        container.appendChild(digit);
+    }
+}
+
+// function add_listeners(){
+//     document.querySelectorAll("#operators, #digits").forEach(element => {
+//         for(var i = 0; i < element.children.length; i++){
+//             let child = element.children[i]
+//             child.addEventListener("click", function(){
+//                 add_to_input(child.textContent);
+//             })
+//         }
+//     });
+
+
+function add_listeners(){
+    const elements = document.querySelectorAll("#operators > *, #digits > *");
+    for (const child of elements) {
+        child.addEventListener("click", () => addToInput(child.textContent));
+    }
+}
+
+function add_to_input(text){
+    field = document.querySelector("#display");
+    field.value = field.value.concat("", text);
+}
+
+create_digits()
+add_listeners();
 let operand1;
 let operand2;
 let operator;
