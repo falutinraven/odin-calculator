1948ca9d50955f19785803e48d6fd47920652bf3
Falutin Raven <falutinraven@gmail.com>
2024-12-02 15:04:36 -0600

    create project structure and add basic operations + generic operate function

---
+++ src/main.js
@@ -0,0 +1,34 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operand1, operator, operand2){
+    switch (operator){
+        case '+':
+            return add(operand1, operand2);
+        case '-':
+            return subtract(operand1, operand2);
+        case '*':
+            return multiply(operand1, operand2);
+        case '/':
+            return divide(operand1, operand2);
+        default:
+            console.log(`unable to perform operation ${operand1} ${operator} ${operand2}`)
+    }
+}
+
+let operand1;
+let operand2;
+let operator;
